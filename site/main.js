f132c095353783399074bc9c16603ef83d3369ec
aditya <aditya.cowsik@gmail.com>
2018-04-15 19:02:15 -0400

    Removed superfluous commented code

--- site/main.js
+++ site/main.js
@@ -201,13 +201,6 @@ var songBuffer = {
         renewCookie();
     },
 
-    /*pushAudio: function(){
-        this.songHistory.push([this.workingTitle, this.workingDuration, this.workingSrc]);
-    },
-
-    rewindAudio: function(steps){
-
-    },*/
 
     getTitleAndURL: function(){
         xhr = new XMLHttpRequest();
@@ -236,14 +229,6 @@ var songBuffer = {
     }
 };
 
-//var canvas = document.getElementById("myCanvas1");
-//canvas.addEventListener("mouseup", handleMouseUp);
-
-//canvas.width = window.innerWidth;
-//canvas.width = window.innerWidth;
-
-// var play = new Shape([ [[0,0], [.15, 0.075], [0, .15]] ], canvas.width/2, canvas.height/3*2);
-// var pause = new Shape([ [[0,0], [.06, 0], [.06,.12], [0, .12]], [[.09, 0], [.15, 0], [.15, .12], [.09, .12]] ])
 
 songBuffer.getTitleAndURL();
 songBuffer.switchAudio();
@@ -267,33 +252,10 @@ function renewCookie(){
 
 draw();
 
-/*function interpolateShapes(shape1, shape2, factor, clip = true){
-    if(clip){
-        factor = Math.max(Math.min(factor, 1), 0);
-    }
-    shape3 = [];
-    for(i = 0; i < shape2.length; i++){
-        t = [0, 0];
-        t[0] = shape1[i][0] * (1-factor) + shape2[i][0] * factor;
-        t[1] = shape1[i][1] * (1-factor) + shape2[i][1] * factor;
-        shape3.push(t);
-    }
-
-    return shape3;
-}*/
 
 function switchPlay(){
     var play = document.getElementById("PlayButton");
     var pause = document.getElementById("PauseButton");
-
-    /*var tmpPE = play.style.pointerEvents;
-    var tempVis = play.style.visibility;
-
-    play.style.pointerEvents = pause.style.pointerEvents;
-    play.style.visibility = pause.style.visibility;
-
-    pause.style.pointerEvents = tmpPE;
-    pause.style.visibility = tempVis;*/
 
     if(!songBuffer.workingAudio.paused){
         songBuffer.workingAudio.pause();
